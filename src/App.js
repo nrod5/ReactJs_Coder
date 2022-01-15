cb64352cbdf66705a665e8480612de35df014492
Nahuel Rodriguez <nahuel.rodriguezcr@gmail.com>
2022-01-15 14:31:42 -0300

    app React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Seccion from './Seccion';
 
 function App() {
   return (
@@ -18,7 +19,10 @@ function App() {
           Learn React
         </a>
       </header>
+    
     </div>
+
+    
   );
 }
 
